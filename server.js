f2262e886b058cf9a24639d21fcb9ff4e55e5145
Smlcrp <smlcrp@gmail.com>
2022-01-14 15:21:47 -0500

    updated create, show, and index to render a page

--- server.js
+++ server.js
@@ -7,25 +7,39 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: false }));
 
 // db
-const products = require('./products');
+const products = require('./models/productModel.js');
 
 // product index route
 app.get('/products', (req, res) => {
-  res.send(products);
-});
-
-// product show route
-app.get('/products/:id', (req, res) => {
-  res.send(products[req.params.id]);
+  const allProducts = products.find();
+  const context = {products: allProducts}
+  res.render('index.ejs', context);
 });
 
 // product create route
+
+app.get("/products/new", function(req, res) {
+  res.render("create.ejs")
+})
+
 app.post('/products', (req, res) => {
   console.log('CREATE route accessed');
   console.log('Data within req.body: ', req.body);
-  products.push(req.body);
+  products.create(req.body, (error, createdProduct) => {
+    if(error) {
+      console.log(error)
+    }
+  });
   res.redirect('/products');
 });
+
+// product show route
+app.get('/products/:productId', (req, res) => {
+  products.findById(req.params.productId, (error, foundProduct) => {
+    res.render('show.ejs', {product: foundProduct})
+  })
+});
+
 
 
 app.listen(PORT, () => {
